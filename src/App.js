33c3f38098c2b55e4f092bc80257ee1e3dd65b76
Alejandra Soto Arevalo <asoto89@gmail.com>
2019-05-24 09:40:49 +0200

    character list done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,61 @@
 import React from 'react';
 import './App.css';
-import { fetchData } from './DataServices/DataServices';
+import {fetchData} from './DataServices/DataServices';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super (props);
     this.state = {
-      characters : []
-    }
+      characters: [],
+    };
   }
 
   componentDidMount () {
-    this.fetchCharacters();
+    this.fetchCharacters ();
   }
 
   fetchCharacters () {
-    fetchData()
-    .then (data => {
-      this.setState({characters: data})
-      })
+    fetchData ().then (data => {
+      const newData = data.map ((item, index) => {
+        return {...item, id: `hp-${index}`};
+      });
+      this.setState ({characters: newData});
+    });
   }
 
   render () {
-  return (
-    <div className="App">
+    const {characters} = this.state;
+    const charactersBuilder = characters.map (character => {
+      return (
+        <li
+          key={character.id}
+          id={character.id}
+          className="character__container"
+        >
+          <div
+            className="character__photo"
+            style={{backgroundImage: `url(${character.image})`}}
+          />
+          <h2 className="character__title">{character.name}</h2>
+          <p className="character__subtitle">{character.house}</p>
+        </li>
+      );
+    });
 
-    </div>
-  );
-}
+    return (
+      <div className="App">
+
+        <div className="Home">
+          <section className="search__result">
+            <ul className="character__list">
+              {charactersBuilder}
+            </ul>
+          </section>
+        </div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
